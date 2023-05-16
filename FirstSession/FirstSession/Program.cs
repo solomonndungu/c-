3b97311ca12f1fd608def5d359a21a67b7eddbe2
solomonndungu <ssouljabee@gmail.com>
2023-05-17 00:37:38 +0300

    Brian classes

---
+++ FirstSession/FirstSession/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class FirstSession
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Hello World");
+    }
+}
+
