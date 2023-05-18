b00f4e32987abb74b481fffbefb5e273c818da08
solomonndungu <ssouljabee@gmail.com>
2023-05-18 15:05:43 +0300

    Prime numbers

--- BrianClass/FirstSession/FirstSession/Program.cs
+++ BrianClass/FirstSession/FirstSession/Program.cs
@@ -12,10 +12,13 @@ public class FirstSession
 }
 
 // Naming convention of variables in c#
+// https://www.informit.com/articles/article.aspx?p=24472
+
 
 // Write a program that displays prime numbers between 0 - 100
 
 // Create a console project that inputs 2 numbers (a, b) and can
 // multiply the two. Using a system library
+
 
 // Difference between write and writeline
---
+++ BrianClass/PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿// Write a program that displays prime numbers between 0 - 100
+
+public class PrimeNumbers
+{
+    public static void Main()
+    {
+        // Write does not add a new line, so you have to include the \n
+        Console.Write("Prime numbers between 1 and 100 are:\n");
+
+        // Loop to check for each number in the range
+        for (int i = 2; i <= 100; ++i)
+        {
+            // Maintain factor count
+            int ctr = 0;
+
+            // Checking for factors
+            for (int j = 2; j <= Math.Sqrt(i); ++j)
+            {
+                if (i % j == 0)
+                {
+                    // Increase factor count when found
+                    ctr = 1;
+                }
+            }
+
+            // Checking and printing prime numbers
+            if (ctr == 0)
+            {
+                // WriteLine can add a new line
+                Console.WriteLine($"{i} ");
+            }
+        }
+
+    }
+}
