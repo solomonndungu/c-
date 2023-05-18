ac895f234c864941e126f2b6af742162389b047d
solomonndungu <ssouljabee@gmail.com>
2023-05-18 19:35:31 +0300

    Explain why math.sqrt

--- BrianClass/PrimeNumbers/PrimeNumbers/Program.cs
+++ BrianClass/PrimeNumbers/PrimeNumbers/Program.cs
@@ -14,6 +14,8 @@ public class PrimeNumbers
             int ctr = 0;
 
             // Checking for factors
+            // https://stackoverflow.com/questions/5811151/why-do-we-check-up-to-the-square-root-of-a-number-to-determine-if-the-number-is#:~:text=To%20test%20whether%20a%20number,square%20root%20of%20that%20number%3F&text=because%20if%20n%20%3D%20a*b,%3D%20a*b%20%3D%20n%20.&text=To%20clarify%2C%20this%20means%20we,floor(sqrt(n))%20.
+            // Above link to explain why we're using square root
             for (int j = 2; j <= Math.Sqrt(i); ++j)
             {
                 if (i % j == 0)
