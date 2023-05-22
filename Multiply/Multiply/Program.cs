d51a250a01bc282fd4124f95b673cad945a8c3e9
solomonndungu <ssouljabee@gmail.com>
2023-05-22 21:01:52 +0300

    Multiplication program

--- Multiply/Multiply/Program.cs
+++ Multiply/Multiply/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿public class Program
+{
+    public static void Main()
+    {
+        int num1, num2, result;
+
+        Console.WriteLine("Enter first number:");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        result = num1 * num2;
+
+        Console.WriteLine("The product of two numbers is:" + result);
+    }
+}
