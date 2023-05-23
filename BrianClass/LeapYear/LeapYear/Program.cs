a1fc2d9e733f795869045d2b81ceb275c6b0c39f
solomonndungu <ssouljabee@gmail.com>
2023-05-23 23:51:32 +0300

    Successful leap year code

--- BrianClass/LeapYear/LeapYear/Program.cs
+++ BrianClass/LeapYear/LeapYear/Program.cs
@@ -1,16 +1,37 @@
 ﻿class LeapYear
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
         Console.WriteLine("Enter Year : ");
-        int Year = Convert.ToInt32(Console.ReadLine());
-        if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        if(IsLeapYear(year))
         {
-            Console.WriteLine("{0} is a leap year", Year); // return true;
+            Console.WriteLine($"{year} is a leap year");
         }
         else
         {
-            Console.WriteLine("{0} is not a leap year", Year);//return false;
+            Console.WriteLine($"{year} is not a leap year");
+        }
+     
+    }
+
+    public static bool IsLeapYear(int Year)
+    {
+        if ((Year % 4 == 0) && (Year % 100 != 0) || (Year % 400 == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
+
+// Different types of programming (OOP, procedural, structural)
+
+// List different access modifiers and extend c# with the different access modifiers
+// Variables stating the different access modifiers listed
+
+// Assigning to constructor of the object when assigning
