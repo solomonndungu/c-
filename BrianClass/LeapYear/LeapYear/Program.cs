e3d2b7deec92bc19401ef8aa023c81032d20587b
solomonndungu <ssouljabee@gmail.com>
2023-05-23 16:25:08 +0300

    Leap Year

---
+++ BrianClass/LeapYear/LeapYear/Program.cs
@@ -0,0 +1,23 @@
+﻿class LeapYear
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Year : ");
+        int Year = Convert.ToInt32(Console.ReadLine());
+
+      //  static bool IsLeapYear(int Year)
+      //  {
+
+
+        if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+        {
+             Console.WriteLine("{0} is a Leap Year.", Year);// return true
+        }
+        else
+        { 
+             Console.WriteLine("{0} is not a Leap Year.", Year);// return false
+        }
+       // }
+
+    }
+}
--- Multiply/Multiply/Program.cs
+++ Multiply/Multiply/Program.cs
@@ -1,17 +1,44 @@
-﻿public class Program
+﻿namespace Product
 {
-    public static void Main()
+    public class Program
     {
-        int num1, num2, result;
+        public static void Main()
+        {
+            int num1, num2, result;
 
-        Console.WriteLine("Enter first number:");
-        num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter first number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Enter second number:");
-        num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
 
-        result = num1 * num2;
+            result = num1 * num2;
 
-        Console.WriteLine("The product of two numbers is:" + result);
+            Console.WriteLine("The product of two numbers is:" + result);
+        }
+
+        static void Multiply()
+        {
+            Console.WriteLine("Enter first number");
+            // Read number as string
+            string input1 = Console.ReadLine();
+
+            Console.WriteLine("Enter second number");
+            string input2 = Console.ReadLine();
+
+            // Parse input strings into numbers
+            if(int.TryParse(input1, out int number1) && int.TryParse(input2, out int number2))
+            {
+                // Calculate product
+                int product = number1 * number2;
+
+                Console.WriteLine($"The product of {number1} and {number2} is: {product}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter valid integer numbers.");
+            }
+        }
     }
+
 }
