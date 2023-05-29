052003d12ccd05e367f07ce1aa99888c941e8b99
solomonndungu <ssouljabee@gmail.com>
2023-05-29 20:42:15 +0300

    Using accessors in classes

--- BrianOpiyoClass/School/Country/Citizen.cs
+++ BrianOpiyoClass/School/Country/Citizen.cs
@@ -3,12 +3,27 @@ namespace Country
 {
 	public class Citizen
 	{
+        private int IdNumber;
+
         private DateTime dateOfBirth;
 
         public int Age()
         {
             DateTime now = DateTime.Now;
             return (now - dateOfBirth).Days / 365;
+        }
+
+        protected string Gender;
+
+        internal string Address;
+
+        protected internal bool VoterRegistered;
+
+        private protected string TaxNumber;
+
+        public int GetIdentityNumber()
+        {
+            return IdNumber;
         }
     }
 }
