910e9dc7fba52bd041a8fe97183ef9b540557a40
solomonndungu <ssouljabee@gmail.com>
2023-05-31 22:27:08 +0300

    Data Structures intro

---
+++ Notations/Notations/Program.cs
@@ -0,0 +1,48 @@
+﻿// https://www.codecademy.com/learn/cspath-asymptotic-notation/modules/cspath-asymptotic-notation/cheatsheet#
+// Asymptotic notation - time and space complexity a program takes to run and give an output.
+// It can be a program with a for loop, foreach...
+
+int num1 = 2;
+
+int num2 = 5;
+
+int num3 = num1 * num2;
+
+Console.WriteLine(num3);
+
+// When defining complexity, use variable names rather than values
+// e.g O(N(X))
+
+
+public class Notations
+{
+    public static string ReverseString(string input)
+    {
+        Stack<char> stack = new Stack<char>(input.Length);
+        foreach (char c in input)
+        {
+            stack.Push(c);
+        }
+
+
+
+        string reversed = "";
+        while (!stack.IsEmpty())
+        {
+            reversed += stack.Pop();
+        }
+
+
+
+        return reversed;
+    }
+
+
+
+    public static void Main()
+    {
+        string input = "Hello, World!";
+        string reversed = ReverseString(input);
+        Console.WriteLine(reversed);
+    }
+}
