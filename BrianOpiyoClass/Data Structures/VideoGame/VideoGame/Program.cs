da64e763fe032eef78313d1b44b274b1cb9ae882
solomonndungu <ssouljabee@gmail.com>
2023-07-04 01:18:53 +0300

    Anonymous Object Types

---
+++ BrianOpiyoClass/Data Structures/VideoGame/VideoGame/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace VideoGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setup objects
+            VideoGames game1 = new VideoGames("PC", "World of Warcraft", "Blizzard", "Teen");
+
+            // anonymous object type. No need of calling the class
+            // We can change content of the keys to whatever we like because it is an anonymous type.
+            var gameAnon = new { Console = "PC", Gamffe = "League of Legends", Publisher = "Riot Games", Rating = "Teen" };
+
+            Console.WriteLine(gameAnon.Gamffe);
+
+            // When creating an anonymous type, we create read only
+
+            // Can create an array (Cascade) of anonymous type
+            var animal = new { Name = "Henry", Species = new { Name = "Dog", Noise = "Bark" } };
+
+            Console.WriteLine(animal.Species.Name);
+        }
+    }
+}
---
+++ BrianOpiyoClass/Data Structures/VideoGame/VideoGame/VideoGames.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace VideoGame
+{
+	public class VideoGames
+	{
+		private string console;
+		private string title;
+		private string publisher;
+		private string rating; // ONLY ALLOWED Ratings: E, 10+, Teen, Mature 17+, Adults Only 18+, Rating Pending
+
+		// Same name as class it is contained in. Allow the programmer to set default values, limit instantiation,
+		// and write code that is flexible and easy to read.
+		public VideoGames(string console, string title, string publisher, string rating)
+		{
+			this.title = title;
+			this.console = console;
+			this.publisher = publisher;
+			this.rating = rating;
+		}
+
+		public string GetRating()
+		{
+			return rating;
+		}
+
+		public string Rating
+		{
+			get { return rating; }
+			set { rating = value; }
+		}
+
+		// Coz anonymous type is read only. This is what is generated behind the scenes. Read-only properties.
+		public VideoGameRatingType Title
+		{
+			get;
+		}
+
+		public VideoGameRatingType Publisher
+		{
+			get;
+		}
+
+		public VideoGameRatingType Rating
+		{
+
+		}
+	}
+}
+
