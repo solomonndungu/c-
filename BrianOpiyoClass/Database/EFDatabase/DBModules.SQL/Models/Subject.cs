a67823b8b1e51e7508d34c5e9cbecfaef660b7c8
solomonndungu <ssouljabee@gmail.com>
2023-09-11 16:58:12 +0300

    Subject model added

---
+++ BrianOpiyoClass/Database/EFDatabase/DBModules.SQL/Models/Subject.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DBModules.SQL.Models
+{
+	public class Subject
+	{
+		[Key, MaxLength(50)]
+		public Guid SubjectId { get; set; } = Guid.NewGuid();
+
+		[MaxLength(100)]
+		public string SubjectName { get; set; }
+
+		public int SchoolId { get; set; }
+
+		public int TeacherId { get; set; }
+
+		public int StudentID { get; set; }
+	}
+}
+
